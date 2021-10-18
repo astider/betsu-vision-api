0b626eb61cf69506268039682f58d214711ce88c
astider <astider@gmail.com>
2021-10-19 01:59:56 +0700

    test functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,38 @@
-import * as functions from "firebase-functions";
+import * as express from 'express';
+import { Request, Response } from 'express';
+import * as functions from 'firebase-functions';
+import * as multer from 'multer';
+import vision from '@google-cloud/vision';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const credentials = require('./service-account-key-file.json')
+const app = express();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage, limits: { fileSize: 5000000 } });
+
+const client = new vision.ImageAnnotatorClient({ credentials });
+
+async function getTextFromFiles(file: Buffer) {
+  const [result] = await client.textDetection(file);
+  const detections = result.textAnnotations;
+  // const textList = [] as string[];
+  console.log('Text:');
+  if (!detections) return;
+  detections.forEach(text => {
+    console.log(text);
+    // textList.push(text);
+  });
+}
+
+app.get('hello', (req: Request, res: Response) => {
+  return res.send('hello');
+})
+
+app.post('upload', upload.single('receipt'), async (req: Request, res: Response) => {
+  const file = req.file;
+  if (!file) return res.json({ success: false });
+  await getTextFromFiles(file.buffer);
+  return res.json({ success: true });
+})
+
+exports.magic = functions.https.onRequest(app);
